b0b58f4e65b34fad0ff8f2d70498566e8415bf0c
Alex <gubanov1514@gmail.com>
2019-09-29 21:27:16 +0300

    Initial Commit

---
+++ Perks for DbD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Perks for DbD
+//
+//  Created by Александр Губанов on 29.09.2019.
+//  Copyright © 2019 Plekhanov University. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Perks for DbDTests/Perks_for_DbDTests.swift
@@ -0,0 +1,34 @@
+//
+//  Perks_for_DbDTests.swift
+//  Perks for DbDTests
+//
+//  Created by Александр Губанов on 29.09.2019.
+//  Copyright © 2019 Plekhanov University. All rights reserved.
+//
+
+import XCTest
+@testable import Perks_for_DbD
+
+class Perks_for_DbDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
